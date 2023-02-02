d734a6dc3862b940974e89f4774e5f28e7229bb0
Edward Fink <finken@us.ibm.com>
2023-02-02 09:16:35 -0500

    initial hello world
    Signed-off-by: Edward Fink <finken@us.ibm.com>

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "sample_var" {
+  description = "A sample_var to pass to the template."
+  default     = "hello world"
+}
+
+variable "sleepy_time" {
+  description = "How long to sleep, in seconds."
+  default     = 1
+}
